1d2e1165eab7358cebea4ae2a0d76d8d1e863552
Leonardo Rodrigues <leonardojrr1@gmail.com>
2020-02-10 21:10:15 -0400

    Colocando los requires del ejecutador de metodos de forma dinamica

--- methodExecuter.js
+++ methodExecuter.js
@@ -1,15 +1,12 @@
 const express = require('express');
 const methodRouter = express.Router();
-const services = {
-    User: require("./services/users")
-};
 
 methodRouter.post("/", async(req, resp, next) => {
 
     let {Class, Method, Params} = req.body
-    let classInstance = Reflect.construct(services[Class],[])
+    let classInstance = Reflect.construct(require(`./services/${Class}`),[])
 
-    let users = await Reflect.apply(classInstance[Method], undefined, [Params]);
+    let users = await Reflect.apply(classInstance[Method], undefined, Params);
 
     resp.send(users)
     
